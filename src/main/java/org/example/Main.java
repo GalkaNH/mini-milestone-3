3934f853c80a3f72fb6fa8e8ed0d885f793b47dc
Galyna <77695117+GalkaNH@users.noreply.github.com>
2023-02-07 14:30:52 +0100

    koniec

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,7 @@ public class Main {
         transportservice.addTransport(new Transport("WSC", "WW5643", 100, Status.NEED_REPAIR, 2021-12-01));
         transportservice.addTransport(new Transport("WAZ", "WW5644", 120, Status.IN_OPERATION, 2021-11-01));
         transportservice.addTransport(new Transport("WDC", "WW5645", 150, Status.IN_OPERATION, 2021-10-01));
+        transportservice.addTransport(new Transport("WDC", "WW5645", 150, Status.IN_OPERATION, 2021-10-01));
     }
 
 }
